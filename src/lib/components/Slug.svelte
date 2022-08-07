8f17634e1672ff7d12fa54191f2ef417e0926b58
Lucas Jansen <wlaj@Macbook.local>
2022-08-07 14:23:54 +0200

    Changed navigation links

--- src/lib/components/Slug.svelte
+++ src/lib/components/Slug.svelte
@@ -7,7 +7,7 @@ export let blok;
 <div use:storyblokEditable={blok}>
    <ul class="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
         <li>
-          <a href={blok.name} class="block py-2 pr-4 pl-3 text-neutral-700 border-b border-neutral-100 hover:bg-neutral-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-neutral-400 md:dark:hover:text-white dark:hover:bg-neutral-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-neutral-700">{blok.name}</a>
+          <a href={blok.link} class="block py-2 pr-4 pl-3 text-neutral-700 border-b border-neutral-100 hover:bg-neutral-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-neutral-400 md:dark:hover:text-white dark:hover:bg-neutral-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-neutral-700">{blok.name}</a>
         </li>
       </ul>
 </div>
--- src/lib/layouts/Footer.svelte
+++ src/lib/layouts/Footer.svelte
@@ -9,7 +9,7 @@
 		class="w-max-5xl mx-auto p-4 bg-white shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-neutral-900/80"
 	>
 		<span class="text-sm text-neutral-500 sm:text-center dark:text-neutral-400"
-			>© 2022 <a href="https://flowbite.com/" class="hover:underline">{blok.name}</a>. All Rights
+			>© 2022 <a href={blok.link} class="hover:underline">{blok.name}</a>. All Rights
 			Reserved.
 		</span>
 		{#each blok.columns as blok}
