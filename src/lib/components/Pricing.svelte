20722e23d91852ddb7265795c78bbee15ccca7d9
Lucas Jansen <wlaj@Macbook.local>
2022-08-12 17:28:53 +0200

    Small changes

--- src/lib/components/Pricing.svelte
+++ src/lib/components/Pricing.svelte
@@ -33,9 +33,4 @@
 			</li>
 		{/each}
 	</ul>
-	<a
-		href="/contact"
-		class="text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:ring-blue-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:text-white  dark:focus:ring-blue-900"
-		>Get started</a
-	>
 </div>
