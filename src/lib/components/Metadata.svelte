5c2ef431c29d02a0342f40a04ab48de2e6c4556b
Lucas Jansen <wlaj@Macbook.local>
2022-08-13 15:07:56 +0200

    Small changes

--- src/lib/components/Metadata.svelte
+++ src/lib/components/Metadata.svelte
@@ -6,9 +6,12 @@
 
 <svelte:head>
 	<div use:storyblokEditable={blok}>
+		<meta charset="UTF-8">
 		<title>{blok.title}</title>
 		<meta name="description" content={blok.description} />
 		<meta name="og:title" property="og:title" content={blok.headline} />
+		<meta name="keywords" content={blok.keyword}>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta name="robots" content="index, follow" />
 	</div>
 </svelte:head>
--- src/lib/layouts/Footer.svelte
+++ src/lib/layouts/Footer.svelte
@@ -9,7 +9,7 @@
 		class="w-max-5xl mx-auto p-4 bg-white shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-neutral-900/80"
 	>
 		<span class="text-sm text-neutral-500 hidden sm:flex sm:text-center dark:text-neutral-400"
-			>© 2022 <a href={blok.link} class="hover:underline">{blok.name}</a>. All Rights
+			>© 2022 {blok.name}. All Rights
 			Reserved.
 		</span>
 		{#each blok.columns as blok}
