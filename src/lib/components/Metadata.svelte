d43a6d81e1e500f2d5b78224ffa5c82809a1ce26
Lucas Jansen <wlaj@Macbook.local>
2022-08-13 15:24:27 +0200

    Small changes

--- src/lib/components/Metadata.svelte
+++ src/lib/components/Metadata.svelte
@@ -9,7 +9,7 @@
 		<meta charset="UTF-8">
 		<title>{blok.title}</title>
 		<meta name="description" content={blok.description} />
-		<meta name="og:title" property="og:title" content={blok.headline} />
+		<meta name="og:title" property="og:title" content={blok.title} />
 		<meta name="keywords" content={blok.keyword}>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta name="robots" content="index, follow" />
