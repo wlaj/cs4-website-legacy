4941e3f1d74f3609e58cab4e65042c0855506226
Lucas Jansen <40787982+wlaj@users.noreply.github.com>
2023-01-23 07:10:36 +0100

    Change static year to dynamic year

--- src/lib/layouts/Footer.svelte
+++ src/lib/layouts/Footer.svelte
@@ -2,6 +2,7 @@
 	import { storyblokEditable, StoryblokComponent } from '@storyblok/svelte';
 
 	export let blok;
+	let year = new Date().getFullYear()
 </script>
 
 <div use:storyblokEditable={blok}>
@@ -9,7 +10,7 @@
 		class="w-max-5xl mx-auto p-4 bg-white shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-neutral-900/80"
 	>
 		<span class="text-sm text-neutral-500 flex sm:text-center dark:text-neutral-400"
-			>© 2022 {blok.name}. All Rights
+			>©{year} {blok.name}. All Rights
 			Reserved.
 		</span>
 		{#each blok.columns as blok}
